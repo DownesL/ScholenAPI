92317685d38d109ecb850abc8bea3bfd140d4fac
DownieTown <lukas.downes@student.odisee.be>
2022-05-15 00:37:54 +0200

    Fetch fix,

--- scripts/form.js
+++ scripts/form.js
@@ -1,18 +1,14 @@
 "use strict";
 
 (function() {
-    const linkBasisScholen = 'https://data.stad.gent/api/records/1.0/search/?dataset=locaties-basisscholen-gent&q=&rows=500';
-    const linkSecundaireScholen = 'https://data.stad.gent/api/records/1.0/search/?dataset=locaties-secundaire-scholen-gent&q=&rows=500';
-    let globalDataBasis;
-    let globalDataSec;
-
     const search = document.querySelector("#search");
 
     const location = document.querySelector("#location");
-    const type = document.querySelector("#type");
     const offer = document.querySelector("#offer");
     const method = document.querySelector("#method");
     const form = document.querySelector("form");
+    const primary = document.querySelector("#primary");
+    const secondary = document.querySelector("#secondary")
 
     const errlocation = document.querySelector("#locationErr");
     const errType = document.querySelector("#typeErr");
@@ -34,43 +30,18 @@
             console.log('Form is valid');
             searchArea.classList.toggle('hidden');
             resultList.classList.toggle('hidden');
-            fetchData();
+            fetchData2();
             //resetForm();
         } else {
             console.log('Form is invalid')
         } 
     })
-
-    type.addEventListener('focusout',()=> {
-        const scndOfferArr = ["SO", "BuSO", "DBSO"];
-        const frstOfferArr = ["BA", "BuO","KO","LO"];
-
-        if(type.value === "Secondary") {
-            scndOfferArr.forEach((el) => {
-                document.querySelector(`option[value="${el}"]`).removeAttribute("disabled");
-            })
-            frstOfferArr.forEach((el) => {
-                document.querySelector(`option[value="${el}"]`).setAttribute("disabled", "");
-            })
-        }
-        if (type.value === "Primary") {
-            scndOfferArr.forEach((el) => {
-                document.querySelector(`option[value="${el}"]`).setAttribute("disabled", "");
-            })
-            frstOfferArr.forEach((el) => {
-                document.querySelector(`option[value="${el}"]`).removeAttribute("disabled");
-            })
-        } 
-        if(type.value === "any") {
-            scndOfferArr.forEach((el) => {
-                document.querySelector(`option[value="${el}"]`).removeAttribute("disabled");
-            })
-            frstOfferArr.forEach((el) => {
-                document.querySelector(`option[value="${el}"]`).removeAttribute("disabled");
-            })
-        }
-        offer.value = 0;
-        method.value = 0;
+    
+    primary.addEventListener('click', ()=>{
+        setSelectOptions()
+    });
+    secondary.addEventListener('click', ()=>{
+        setSelectOptions()
     });
 
     resultBtn.addEventListener('click', () => {
@@ -82,11 +53,14 @@
     });
     tagOffers.addEventListener('click', () => {
         document.querySelector(".aanbodLegende").classList.toggle("hidden");
+        document.querySelector(".netLegende").classList.add("hidden");
     });
     netTags.addEventListener('click',() => {
         document.querySelector(".netLegende").classList.toggle("hidden");
+        document.querySelector(".aanbodLegende").classList.add("hidden");
     })
 
+    //FORM VALIDATION
     const validateForm = ()=> {
         let isValid = true;
         if (location.value === '0') {
@@ -97,8 +71,8 @@
             setError(errMethod,'Geen methode meegegeven, vul methode in');
             isValid = false;
         }
-        if (type.value === "0") {
-            setError(errType,'Geen type meegegeven, vul type in');
+        if (!primary.checked && !secondary.checked) {
+            setError(errType,'Geen graad meegegeven, vul graad in');
             isValid = false;
         }
         if (offer.value === "0") {
@@ -121,100 +95,203 @@
         location.value = 0;
         offer.value = 0;
         method.value = 0;
-        type.value = 0;
+        primary.checked = false;
+        secondary.checked = false;
         
     }
+    //DISPLAYING ALL THE FETCHED DATA
+    const createHtml = (el) => {
+        const result = document.createElement("section");
+        //HEADER
+        const header = document.createElement("h2");
+        const headerText = document.createTextNode(`${el.fields.naam}`)
+        //LOCATION
+        const locationHeader = document.createElement('h3');
+        const locationHeaderText = document.createTextNode(`${el.fields.gemeente}`);
+        //TAGS
+        const tags = document.createElement("div");
+        const offerTag = document.createElement("span");
+        const offerTagText = document.createTextNode(`${el.fields.aanbod}`);
+        const netTag = document.createElement("span");
+        const netTagText = document.createTextNode(`${el.fields.onderwijsnet}`);
+        //DESCRIPTION
+        const para = document.createElement("p");
+        const paraText = document.createTextNode("Deze school is heel mooi en heel goed")
+        //WEBSITE
+        const web = document.createElement("a")
+        const webLink = `${el.fields.website}`;
+        const webText = document.createTextNode("Website: " + el.fields.naam);
+        //RATING
+        const ratingDiv = document.createElement("div");
+        const ratingSpan = document.createElement("span");
+        const ratingSpanText = document.createTextNode("Score: * * * * *");
 
-    const displayData = () => {
-        let data;
-        if(type.value === "Primary") {
-            data = globalDataBasis.records.filter(el =>(
-                (offer.value === "any" ? true : el.fields.aanbod === offer.value) &&
-                    (method.value === "any" ? true : el.fields.onderwijsnet === method.value) &&
-                    (location.value === "any" ? true : el.fields.gemeente === location.value)
-            ))
-        }
-        if(type.value === "Secondary") {
-            data = globalDataSec.records.filter(el =>(
-                (offer.value === "any" ? true : el.fields.aanbod === offer.value) &&
-                    (method.value === "any" ? true : el.fields.onderwijsnet === method.value) &&
-                    (location.value === "any" ? true : el.fields.gemeente === location.value)
-            ))
-        }
-        if(type.value === "any") {
-            let dataBasisTemp = globalDataBasis.records.filter(el =>(
-                (offer.value === "any" ? true : el.fields.aanbod === offer.value) &&
-                    (method.value === "any" ? true : el.fields.onderwijsnet === method.value) &&
-                    (location.value === "any" ? true : el.fields.gemeente === location.value)
-            ))
-            let dataSecTemp = globalDataSec.records.filter(el =>(
-                (offer.value === "any" ? true : el.fields.aanbod === offer.value) &&
-                    (method.value === "any" ? true : el.fields.onderwijsnet === method.value) &&
-                    (location.value === "any" ? true : el.fields.gemeente === location.value)
-            ))
-            data = dataBasisTemp.concat(dataSecTemp);
-        }
-        if(search.value !== "") {
-            data = data.filter(el => (
-                el.fields.naam.search(search.value) !== -1
-            ))
-        }
-        console.log(data);
+        //HEADER
+        header.appendChild(headerText);
+        //TAGS
+        tags.appendChild(offerTag);
+        tags.appendChild(netTag);
+        offerTag.appendChild(offerTagText)
+        netTag.appendChild(netTagText)
+        //LOCATION
+        locationHeader.appendChild(locationHeaderText);
+        //DESCRIPTION
+        para.appendChild(paraText);
+        //WEBSITE
+        web.setAttribute("href",webLink);
+        web.appendChild(webText);
+        //RATING
+        ratingDiv.appendChild(ratingSpan);
+        ratingSpan.appendChild(ratingSpanText);
+        //SECTION
+        result.appendChild(header);
+        result.appendChild(tags);
+        result.appendChild(locationHeader);
+        result.appendChild(para);
+        result.appendChild(web);
+        result.appendChild(ratingDiv);
+        resultList.appendChild(result);
 
-        data.forEach((el) => {
-            const result = document.createElement("div");
-            const header = document.createElement("h2");
-            const headerText = document.createTextNode(`${el.fields.naam}`)
-            const locationHeader = document.createElement('h3');
-            const locationHeaderText = document.createTextNode(`${el.fields.gemeente}`);
-            const tags = document.createElement("div");
-            const offerTag = document.createElement("span");
-            const offerTagText = document.createTextNode(`${el.fields.aanbod}`);
-            const netTag = document.createElement("span");
-            const netTagText = document.createTextNode(`${el.fields.onderwijsnet}`);
-            const para = document.createElement("p");
-            const paraText = document.createTextNode("Deze school is heel mooi en heel goed")
-            const ratingDiv = document.createElement("div");
-            const ratingSpan = document.createElement("span");
-            const ratingSpanText = document.createTextNode("Score: * * * * *");
+        //ADD CLASSNAMES
+        result.className = "result";
+        tags.className = "tags";
+        offerTag.className = "tag offerTag";
+        netTag.className = "tag netTag";
+        para.className = "description";
+        ratingDiv.className = "rating";
+    }
+    //CHANGING THE POSSIBLE OPTION ACCORDING TO PRIMARY/SECONDARY SCHOOL
+    const setSelectOptions = () => {
+        const scndOfferArr = ["SO", "BuSO", "DBSO"];
+        const frstOfferArr = ["BA", "BuO","KO","LO"];
 
-            tags.appendChild(offerTag);
-            tags.appendChild(netTag);
-            offerTag.appendChild(offerTagText)
-            netTag.appendChild(netTagText)
-            para.appendChild(paraText);
-            ratingDiv.appendChild(ratingSpan);
-            ratingSpan.appendChild(ratingSpanText);
-            result.appendChild(header);
-            header.appendChild(headerText);
-            result.appendChild(tags);
-            result.appendChild(locationHeader);
-            locationHeader.appendChild(locationHeaderText);
-            result.appendChild(para);
-            result.appendChild(ratingDiv);
-            resultList.appendChild(result);
-            result.className = "result";
-            tags.className = "tags";
-            offerTag.className = "tag offerTag";
-            netTag.className = "tag netTag";
-            para.className = "description";
-            ratingDiv.className = "rating";
-        })
+        if(secondary.checked && !primary.checked) {
+            scndOfferArr.forEach((el) => {
+                document.querySelector(`option[value="${el}"]`).removeAttribute("disabled");
+            })
+            frstOfferArr.forEach((el) => {
+                document.querySelector(`option[value="${el}"]`).setAttribute("disabled", "");
+            })
+        }
+        if (primary.checked && !secondary.checked) {
+            scndOfferArr.forEach((el) => {
+                document.querySelector(`option[value="${el}"]`).setAttribute("disabled", "");
+            })
+            frstOfferArr.forEach((el) => {
+                document.querySelector(`option[value="${el}"]`).removeAttribute("disabled");
+            })
+        } 
+        if(secondary.checked && primary.checked) {
+            scndOfferArr.forEach((el) => {
+                document.querySelector(`option[value="${el}"]`).removeAttribute("disabled");
+            })
+            frstOfferArr.forEach((el) => {
+                document.querySelector(`option[value="${el}"]`).removeAttribute("disabled");
+            })
+        }
+        offer.value = 0;
+        method.value = 0;
+    }
+    //ERROR MESSAGE IF NO RESULTS WERE FOUND
+    const setZeroResults = () => {
+        const result = document.createElement("section");
+        //HEADER
+        const header = document.createElement("h2");
+        const headerText = document.createTextNode("Geen resultaten gevonden");
+
+        //DESCRIPTION
+        const para = document.createElement("p");
+        const paraText = document.createTextNode("Je zoekresultaten leverden geen resultaten op.");
+        
+        //HEADER
+        header.appendChild(headerText);
+        //DESCRIPTION
+        para.appendChild(paraText);
+        //SECTION
+        result.appendChild(header);
+        result.appendChild(para);
+        resultList.appendChild(result);
+
+        //ADD CLASSNAMES
+        result.className = "result noResults";
+        para.className = "description";
     }
 
-    const fetchData = async () => {
+    let displaySet = 0;
+    let globalDataBasis2, globalDataSec2;
+    const fetchData2 = async () => {
+        const searchQuery = (search.value === "" ? "" : "naam%3D"+search.value);
+        
+        const queryStart = (displaySet*10)
+        const rows = (primary.checked && secondary.checked ? "&rows=5" : "&rows=10")
+        const locatieQuery = (location.value === "any" ? "" : "&refine.gemeente=" + location.value);
+        const aanbodQuery = (offer.value === "any" ? "" : "&refine.aanbod=" + offer.value);
+        const netQuery = (method.value === "any" ? "" : "&refine.onderwijsnet=" + method.value);
+        const linkBasisScholen2 = `https://data.stad.gent/api/records/1.0/search/?dataset=locaties-basisscholen-gent&q=${searchQuery}&start=${queryStart}${aanbodQuery}${netQuery}${locatieQuery}${rows}`
+        const linkSecundaireScholen2 = `https://data.stad.gent/api/records/1.0/search/?dataset=locaties-secundaire-scholen-gent&q=${searchQuery}&start=${queryStart}${aanbodQuery}${netQuery}${locatieQuery}${rows}`
+    
         try {
-            let responseBasis = await fetch(linkBasisScholen);
+            let responseBasis = await fetch(linkBasisScholen2);
             if(!responseBasis.ok) throw Error();
-            globalDataBasis = await responseBasis.json();
-            let responseSec = await fetch(linkSecundaireScholen);
+            globalDataBasis2 = await responseBasis.json();
+            let responseSec = await fetch(linkSecundaireScholen2);
             if (!responseSec.ok) throw Error();
-            globalDataSec = await responseSec.json();
-            displayData();
+            globalDataSec2 = await responseSec.json();
         }
         catch (err) {
             console.log(err)
         }
+        try {
+            if(primary.checked && !secondary.checked) {
+                if (globalDataBasis2.nhits === 0) throw Error();
+                globalDataBasis2.records.forEach((el) => createHtml(el));
+            }
+            if(!primary.checked && secondary.checked) {
+                if (globalDataSec2.nhits === 0) throw Error();
+                globalDataSec2.records.forEach((el) => createHtml(el));
+            }
+            if(primary.checked && secondary.checked) {
+                if (globalDataBasis2.nhits === 0 && globalDataSec2.nhits === 0) throw Error();
+                let dataTemp = globalDataBasis2.records.concat(globalDataSec2.records);
+                dataTemp.forEach((el) => createHtml(el));
+            }
+        } catch(err) {
+            console.log(err);
+            setZeroResults();
+        }
+        
 
     }
+
+    /**
+     * This part of the js is for the infinite scroll effect    
+     */
+
+    let lastKnownScrollPosition = 0;
+    let ticking = false;
+    let scrollCount = 1;
+
+    function moooooooreData(scrollPos) {
+        if (scrollPos >= scrollCount*1600 + (scrollCount-1)*200 + 1500) {
+            fetchData2();
+            displaySet++;
+            scrollCount++;
+        }
+    }
+
+    document.addEventListener('scroll', function(e) {
+        lastKnownScrollPosition = window.scrollY;
+
+        if (!ticking) {
+            window.requestAnimationFrame(function() {
+                moooooooreData(lastKnownScrollPosition);
+                ticking = false;
+            });
+            ticking = true;
+        }
+    });
+
+
+
+
 })();
